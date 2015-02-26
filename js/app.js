7c69b5b3cb2621c809bda8629b262e5cdab05189
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-02-25 22:02:24 -1000

    update to latest version

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ $( document ).ready (function() {
 
     $.each(data.data.children, function(idx, val) {
 
-      console.log(val);
+//      console.log(val);
 
       var element = $("<div>");
       element.addClass("item column small-12 medium-6");
@@ -16,10 +16,19 @@ $( document ).ready (function() {
       var e_image = $("<div>");
       e_image.addClass("image");
       e_image.css("background-image", "url(" + val.data.url+ ")");
+
+        var e_enclosed_image = $("<img>");
+        e_enclosed_image.attr("src", val.data.url);
+        e_enclosed_image.css("width", "100%");
+        e_enclosed_image.css("height", "100%");
+        e_enclosed_image.hide();
+        e_image.append(e_enclosed_image);
+
+
       element.append(e_image);
 
       var e_title = $("<h2>");
-      e_title.addClass("title");
+      e_title.addClass("title clamp-2");
       e_title.html(val.data.title);
       element.append(e_title);
 
@@ -47,8 +56,10 @@ $( document ).ready (function() {
       element.append(e_subtitle);
 
       var e_desc = $("<div>");
-      e_desc.addClass("description");
-      e_desc.html("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum et, iste quae quibusdam.");
+      e_desc.addClass("description clamp-3");
+      var count = Math.floor(Math.random() * 100 + 1);
+      e_desc.lorem({type: 'words', amount: count.toString(), ptags: false});
+//      e_desc.html("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum et, iste quae quibusdam.");
       element.append(e_desc);
 
       $(".item_list").append(element);
@@ -58,7 +69,7 @@ $( document ).ready (function() {
   $(".menu_boards").click(
       function() {
         $.getJSON(
-          "./api/my_boards.json",
+          "/api/my_boards.json",
           "",
           processData
           );
@@ -70,7 +81,7 @@ $( document ).ready (function() {
   $(".menu_random").click(
       function() {
         $.getJSON(
-          "./api/random.json",
+          "/api/random.json",
           "",
           processData
           );
@@ -80,7 +91,7 @@ $( document ).ready (function() {
   $(".menu_app").click(
       function() {
         $.getJSON(
-          "./api/get_the_app.json",
+          "/api/get_the_app.json",
           "",
           processData
           );
